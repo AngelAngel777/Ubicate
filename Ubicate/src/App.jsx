22f0ea8f4b168f408145a1dcfd4323b403480ef0
informatica-237 <pruebasinformatica237@gmail.com>
2024-01-24 09:36:07 -0300

    Firebase y un Boton de prueba

---
+++ Ubicate/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <>
+      <Button variant="info">Hola Mundo</Button>
+    </>
+  )
+}
+
+export default App
